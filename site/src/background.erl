ef2c9f97b28e1c1c03cc26639bbe75cf87caa1df
Angel Alvarez <angeljalvarezmiguel@gmail.com>
2014-12-07 21:40:38 +0100

    interim refactor of background module

--- site/src/background.erl
+++ site/src/background.erl
@@ -20,27 +20,33 @@
     }).
 
 %% Wait for INIT message and setup a proper state, the go idle
-background_update_init(Control, Panel) ->
-%%    io:format("background_update_init: updating element %s in panel %s\n",[Control, Panel]),
-    receive 
-        'INIT' ->
-            background_update_idle(#bar_state{ id = Control, panel = Panel, severity = success, count = 0})
-    end.
+background_update_init(Control, Panel, Count) ->
+    io:format("Hey! I'm a comet process updating ~p in panel ~p \n",[Control, Panel]),
+    background_update_idle(#bar_state{ id = Control, panel = Panel, severity = success, count = Count}).
 
 %% Block until desired panel get visible
-background_update_idle(#bar_state{ panel = ThisPanel} = State) -> 
-    io:format("background_update: idle\n",[]),
+background_update_idle(#bar_state{ id = Control, panel = ThisPanel} = State) -> 
     receive
+        'INIT' ->
+                        io:format("background_update_idle: I'm process for ~p and I am the first in the pool..\n",[Control]),
+                        background_update_idle(State);        
         {panel, ThisPanel} ->
-                        background_update(State)
+                        io:format("background_update_idle: got a {~p, ~p} msg, going to ACTIVE..\n",[panel,ThisPanel]),
+                        background_update(State);
+        Other ->
+                        io:format("background_update_idle: got a ~p msg, going to IDLE..\n",[Other]),
+                        background_update_idle(State)        
     end.
 %% Loop updating state unless we receive a new panel event, go to idle if apropiate (our panel is no longer visible)
 background_update(#bar_state{ panel = ThisPanel } = State) ->
+    io:format("background_update_idle: ACTIVE for panel ~p..\n",[ThisPanel]),
     receive
         {panel, Panel} when Panel =/= ThisPanel ->
+                io:format("background_update: got a {~p, ~p} msg, going to idle..\n",[panel,Panel]),
                 background_update_idle(State)
     after
         1000 ->
+                io:format("background_update: looping\n",[]),
                 NewState = update_state(State),
                 background_update(NewState)
     end.
