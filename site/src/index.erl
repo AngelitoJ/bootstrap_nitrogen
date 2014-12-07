9893d708629fd14d52654b6e060410e34cb4d0fe
Angel Alvarez <angeljalvarezmiguel@gmail.com>
2014-12-07 14:28:38 +0100

    Place a new welcome msg on index page

--- site/src/index.erl
+++ site/src/index.erl
@@ -18,26 +18,31 @@ body() ->
                     }.
 
 inner_body() -> 
-    [
-         #h1 { text="Welcome to Nitrogen"}
-        ,#h1 { text="(Bootstrap playground)" }
-        ,#p{}
-        ,<<"If you can see this page, then your Nitrogen server is up and running. Click the button below to start.">>
-        ,#p{} 	
-        ,#button { id=button, text="Click me!", postback=click }
-		,#p{}
-    ].
+        #panel{  id = msgPanel
+                ,body = [
+                            #h1{ text = "Nitrogen Bootstrap playground" }
+                            ,#p{ text = "Playground is a toy project to feature current Nitrogen capabilities and provide a test bed to develop Bootstrap components upon nitrogen DSL."}
+                            ,#p{ html_encode = false, text = "<a href=\"nitrogenproject.com\">Nitrogen Project</a> is a erlang web framework geared toward higly dynamic sites."}
+                            ,#p{ html_encode = false, text = "Dashboard is <a rel=\"nofollow\" href=\"http://www.templatemo.com/preview/templatemo_415_dashboard\">free admin template</a> for everyone. Credits go to <a href=\"http://chartjs.org\">Chart JS</a>, <a href=\"http://getbootstrap.com\">Bootstrap</a>, and <a href=\"http://jqvmap.com\">JQVMap</a>. templatemo provides <a href=\"#\">free website templates</a> that can be used for any purpose."}
+                            ,#p{}
+                            ,<<"If you can see this page, then your Nitrogen server is up and running. Click the button below to start.">>
+                            ,#p{} 	
+                            ,#button { id=button, text="Click me!", postback=click }
+		                  ,#p{}
+                        ]
+                }.
 	
 event(click) ->
-    wf:replace(button, #panel { 
+    wf:replace(msgPanel, #panel { 
                                  body=[
-                                        #button { 
+                                        #h1{ text="Ready to see the Dashboard?"}
+                                        ,#button { 
                                                      id=button2
-                                                    ,text="Ready to see dashboard?"
+                                                    ,text="Go!"
                                                     ,postback=click2
                                                 }
                                         ] 
-                                ,actions=#effect { effect=highlight, speed=100 }
+                                ,actions=#effect { effect=slide, speed=100 }
                                 });
 event(click2) -> 
     wf:redirect("dashboard").
